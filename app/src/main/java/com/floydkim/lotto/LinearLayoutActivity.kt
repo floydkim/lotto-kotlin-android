ec293cb2d15c51474d12bde659d156edfc67c210
floydkim <r2d2em@empal.com>
2019-11-06 21:17:39 +0900

    Color 코드 및 각화면 AppTheme 작성 (475p)

---
+++ app/src/main/java/com/floydkim/lotto/LinearLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.floydkim.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinearLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/floydkim/lotto/RelativeLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.floydkim.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
