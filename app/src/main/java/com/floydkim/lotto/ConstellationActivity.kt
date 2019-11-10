19000ccc65cc7180782cffb8eed25b543e581d06
floydkim <r2d2em@empal.com>
2019-11-10 18:52:37 +0900

    DatePicker 선택시 해당 별자리로 TextView 변경되도록 추가

--- app/src/main/java/com/floydkim/lotto/ConstellationActivity.kt
+++ app/src/main/java/com/floydkim/lotto/ConstellationActivity.kt
@@ -3,8 +3,13 @@ package com.floydkim.lotto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.DatePicker
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_constellation.*
+import java.util.Calendar
+
+//import android.icu.util.Calendar // 이건 API level 하위호환 안됨
 
 class ConstellationActivity : AppCompatActivity() {
 
@@ -16,6 +21,47 @@ class ConstellationActivity : AppCompatActivity() {
             startActivity(Intent(this, ResultActivity::class.java))
         }
 
+        textView.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+
+        val calendar = Calendar.getInstance()
+        datePicker.init(
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH),
+            object : CalendarView.OnDateChangeListener, DatePicker.OnDateChangedListener {
+                override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                    textView.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+                }
+
+                override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, dayOfMonth: Int) {
+
+                }
+            })
+
         Toast.makeText(applicationContext, "ConstellationActivity 입니다.", Toast.LENGTH_SHORT).show()
     }
+}
+
+fun makeConstellationString(month: Int, day: Int): String {
+    val target = "${month + 1}${String.format("%02d", day)}".toInt()
+    var constellation = ""
+
+    when (target) {
+        in 101..119 -> constellation = "염소"
+        in 120..218 -> constellation = "물병"
+        in 219..320 -> constellation = "물고기"
+        in 321..419 -> constellation = "양"
+        in 420..520 -> constellation = "황소"
+        in 521..621 -> constellation = "쌍둥이"
+        in 622..722 -> constellation = "게"
+        in 723..822 -> constellation = "사자"
+        in 823..923 -> constellation = "처녀"
+        in 924..1022 -> constellation = "천칭"
+        in 1023..1122 -> constellation = "전갈"
+        in 1123..1224 -> constellation = "사수"
+        in 1225..1231 -> constellation = "염소"
+        else -> constellation = "잘못된 별"
+    }
+
+    return "${constellation}자리"
 }
