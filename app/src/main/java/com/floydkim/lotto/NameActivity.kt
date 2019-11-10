25a3ce33cb9447a521377fead06a2c1963dc2039
floydkim <r2d2em@empal.com>
2019-11-10 17:48:13 +0900

    이름으로 생성시 만든 날짜에 따라 달라지도록 추가 / 이름 빈칸 및 띄어쓰기 입력 예외처리 추가

--- app/src/main/java/com/floydkim/lotto/NameActivity.kt
+++ app/src/main/java/com/floydkim/lotto/NameActivity.kt
@@ -3,8 +3,12 @@ package com.floydkim.lotto
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_name.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 import kotlin.random.Random
 
 class NameActivity : AppCompatActivity() {
@@ -15,10 +19,17 @@ class NameActivity : AppCompatActivity() {
 
         goButton.setOnClickListener {
             val intent = Intent(this, ResultActivity::class.java)
-            val inputText = editText.text.toString()
+            val inputText = editText.text.toString().trim()
+
+            if (TextUtils.isEmpty(inputText)) {
+                Toast.makeText(applicationContext, "이름을 입력해주세요!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val lottoNumbersList = getLottoNumbersFromHash(inputText)
 
             intent.putIntegerArrayListExtra("result", ArrayList(lottoNumbersList))
+            intent.putExtra("name", inputText)
 
             startActivity(intent)
         }
@@ -38,8 +49,11 @@ fun getLottoNumbersFromHash(name: String): MutableList<Int> {
         list.add(number)
     }
 
+    // 같은 이름이라도 오늘이 지나면 다른 번호를 만들기 위함
+    val targetString = SimpleDateFormat("yyyy-MM-dd", Locale.KOREA).format(Date()) + name
+
     // 입력받은 이름을 hashing해 seed로 삼아 shuffle을 수행
-    list.shuffle(Random(name.hashCode().toLong()))
+    list.shuffle(Random(targetString.hashCode().toLong()))
 
     return list.subList(0, 6)
 }
